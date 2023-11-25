4f8833a9bc9349d3ebea11bae30f3fa4a7199035
Frederico <frederico.smozzato@gmail.com>
2023-11-25 08:11:19 -0300

    Adiciona informações de contato para pousadas

--- js/main.js
+++ js/main.js
@@ -15,14 +15,25 @@ const app = Vue.createApp({
                 let inn = new Object();
 
                 inn.name = o.name;
+                inn.phone = o.phone;
+                inn.email = o.email;
+                inn.description = o.description;
+                inn.payMethods = o.pay_methods;
+                inn.petFriendly = o.pet_friendly;
+                inn.userPolicies = o.user_policies;
+                inn.checkInTime = o.formatted_check_in_time;
+                inn.checkOutTime = o.formatted_check_ou_ttime;
+                inn.score = o.average_score;
+                inn.street = o.address.street;
+                inn.number = o.address.number;
+                inn.neighborhood = inn.neighborhood;
                 inn.city = o.address.city;
                 inn.state = o.address.state;
-                inn.description = o.description;
-                inn.score = o.average_score
+                inn.postalCode = o.postal_code;
 
                 this.innsList.push(inn);
             });
-        }
+        },
     },
     beforeMount() {
         this.getInns();
