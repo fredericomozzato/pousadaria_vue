1f025ae6022c0033cb4926d8ac21477aa8f7127b
Frederico <frederico.smozzato@gmail.com>
2023-11-25 07:36:04 -0300

    Cria listagem de pousadas na página inicial

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
-            title: "Pousadaria"
+            innsList: []
         }
+    },
+    methods: {
+        async getInns() {
+            let res = await fetch("http://localhost:3000/api/v1/inns");
+            let data = await res.json();
+
+            console.log(data);
+
+            data.forEach(o => {
+                let inn = new Object();
+
+                inn.name = o.name;
+                inn.city = o.address.city;
+                inn.state = o.address.state;
+                inn.description = o.description;
+                inn.score = o.average_score
+
+                this.innsList.push(inn);
+            });
+        }
+    },
+    beforeMount() {
+        this.getInns();
     }
 });
 
